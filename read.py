8643d45f1e44c320d0ffbcf80de811837f454adb
Bruno Cabral <cabral.brc@gmail.com>
2021-10-06 13:25:05 -0700

    open video

--- read.py
+++ read.py
@@ -2,8 +2,22 @@ import cv2 as cv
 
 # img = cv.imread('Resources/Photos/cat.jpg')
 
-img = cv.imread('Resources/Photos/cat_large.jpg')
+# img = cv.imread('Resources/Photos/cat_large.jpg')
 
-cv.imshow('Cat', img)
+# cv.imshow('Cat', img)
 
-cv.waitKey(0)
+# cv.waitKey(0)
+
+# Reading Videos
+# capture = cv.VideoCapture(0) # 0 param is webcam id
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
