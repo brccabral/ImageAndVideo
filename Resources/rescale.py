e7d6ad9d12c54629f1aa26a09307aee8cad78542
Bruno Cabral <cabral.brc@gmail.com>
2021-10-07 09:14:45 -0700

    rescale exercise

---
+++ Resources/rescale.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+import numpy
+
+def rescaleFrame(frame: numpy.ndarray, scale: float=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+img = cv.imread('Resources/Photos/cat_large.jpg')
+img_resized = rescaleFrame(img)
+cv.imshow('Cat', img)
+cv.imshow('Cat Resized', img_resized)
+
+cv.waitKey(0)
+
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+isTrue = True
+while isTrue:
+    isTrue, frame = capture.read()
+    if not isTrue:
+        break
+    frame_resized = rescaleFrame(frame)
+    cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
