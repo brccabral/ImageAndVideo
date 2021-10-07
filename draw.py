4d5228b4ad82fd6605cde245cac5481f444197db
Bruno Cabral <cabral.brc@gmail.com>
2021-10-07 09:32:23 -0700

    draw circle

--- draw.py
+++ draw.py
@@ -36,4 +36,13 @@ blank[:]=0,0,0 # reset to black
 cv.rectangle(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thickness=cv.FILLED)
 cv.imshow('Square', blank)
 
+# draw circle
+blank[:]=0,0,0 # reset to black
+# np, center, radius, color, thickness
+cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 40, (0,0,255), thickness=3)
+cv.imshow('Circle', blank)
+
+cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 40, (0,0,255), thickness=cv.FILLED)
+cv.imshow('Circle filled', blank)
+
 cv.waitKey(0)
