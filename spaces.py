18952806028c754f55123a7276390dcda4fd40bb
Bruno Cabral <cabral.brc@gmail.com>
2021-10-07 11:52:55 -0700

    spaces to HSV

--- spaces.py
+++ spaces.py
@@ -8,4 +8,8 @@ cv.imshow('Park', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+# BGR to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow('HSV', hsv)
+
 cv.waitKey(0)
