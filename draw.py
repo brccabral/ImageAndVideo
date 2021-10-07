c1b1f3c138e500e479e6049dc9a4e2141c3bd04e
Bruno Cabral <cabral.brc@gmail.com>
2021-10-07 09:29:53 -0700

    paint image with color

--- draw.py
+++ draw.py
@@ -10,4 +10,13 @@ import numpy as np
 blank = np.zeros((500,500,3), dtype='uint8')
 cv.imshow('Blank', blank)
 
+# paint image with color
+#Blue,Green,Red
+
+blank[:] = 0,255,0
+cv.imshow('Green', blank)
+
+blank[:] = 0,0,255
+cv.imshow('Red', blank)
+
 cv.waitKey(0)
