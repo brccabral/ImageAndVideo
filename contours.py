e9389c4ab3143828956f198b9544ac6a5ead405e
Bruno Cabral <cabral.brc@gmail.com>
2021-10-07 11:32:04 -0700

    find contours from gray and blur

--- contours.py
+++ contours.py
@@ -24,4 +24,13 @@ print(f'{len(contours)} contours(s) found CHAIN_APPROX_SIMPLE')
 contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 print(f'{len(contours)} contours(s) found CHAIN_APPROX_NONE')
 
+# blur
+blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges Blur', canny)
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+print(f'{len(contours)} contours(s) found blur CHAIN_APPROX_SIMPLE')
+
+
 cv.waitKey(0)
