173ad6ac95f00e518bfaa8cefc3e28404c857221
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 11:02:30 -0700

    detect faces in larger group

--- face_detect.py
+++ face_detect.py
@@ -17,10 +17,10 @@ cv.imshow('Detected faces', img)
 
 img_group2: np.ndarray = cv.imread('Resources/Photos/group 2.jpg')
 img = img_group2.copy()
-cv.imshow('Group', img)
+cv.imshow('Group2', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Group Gray', gray)
+cv.imshow('Group2 Gray', gray)
 
 haar_cascade: cv.CascadeClassifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
@@ -28,7 +28,7 @@ print(f'Number of faces found = {len(faces_rect)}')
 
 for (x,y,w,h) in faces_rect:
     cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
-cv.imshow('Group Detected faces', img)
+cv.imshow('Group2 Detected faces', img)
 
 
 img = img_group2.copy()
@@ -38,6 +38,24 @@ print(f'Number of faces found = {len(faces_rect)}')
 
 for (x,y,w,h) in faces_rect:
     cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
-cv.imshow('Group Detected faces minNeighbors=6', img)
+cv.imshow('Group2 Detected faces minNeighbors=6', img)
+
+
+
+img_group1: np.ndarray = cv.imread('Resources/Photos/group 1.jpg')
+img = img_group1.copy()
+cv.imshow('Group1', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Group1 Gray', gray)
+
+haar_cascade: cv.CascadeClassifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+print(f'Number of faces found = {len(faces_rect)}')
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+cv.imshow('Group1 Detected faces', img)
+
 
 cv.waitKey(0)
