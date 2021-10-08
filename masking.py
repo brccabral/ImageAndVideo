74a6d070d37f0dd20ff025eb312a17c1abb0383c
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 09:41:16 -0700

    Masked Rectangle Image

--- masking.py
+++ masking.py
@@ -5,10 +5,16 @@ img: np.ndarray = cv.imread('Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
 blank = np.zeros(img.shape[:2], dtype='uint8')
-mask = cv.circle(blank, (img.shape[1]//2+60,img.shape[0]//2), 100, 255, -1)
+mask = cv.circle(blank.copy(), (img.shape[1]//2+60,img.shape[0]//2), 100, 255, -1)
 cv.imshow('Mask circle', mask)
 
 masked = cv.bitwise_and(img, img, mask=mask)
 cv.imshow('Masked Circle Image', masked)
+
+mask = cv.rectangle(blank.copy(), (img.shape[1]//2-60,img.shape[0]//2), (img.shape[1]//2+70,img.shape[0]//2+100), 255, -1)
+cv.imshow('Mask rectangle', mask)
+
+masked = cv.bitwise_and(img, img, mask=mask)
+cv.imshow('Masked Rectangle Image', masked)
 
 cv.waitKey(0)
