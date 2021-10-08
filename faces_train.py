eb7d568284d9e92c794c46d80404470d636c8d12
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 11:09:30 -0700

    check if it can loop through the train images

--- faces_train.py
+++ faces_train.py
@@ -2,7 +2,24 @@ import os
 import cv2 as cv
 import numpy as np
 
-p = []
-for i in os.listdir(r'./Resources/Faces/train'):
-    p.append(i)
-print(p)
+DIR = r'./Resources/Faces/train'
+
+people = []
+for i in os.listdir(DIR):
+    people.append(i)
+print(people)
+
+features = []
+labels = []
+
+def create_train():
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path, img)
+            print(img_path)
+            img_array = cv.imread(img_path)
+
+create_train()
