37482affb31432ea206145a9dcd298d30129365f
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 11:41:15 -0700

    show detected name and location on image

--- Resources/face_recognition.py
+++ Resources/face_recognition.py
@@ -12,16 +12,24 @@ face_recognizer.read('face_trained.yml')
 
 people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
 
-img = cv.imread(r'Resources/Faces/val/ben_afflek/2.jpg')
-cv.imshow('Ben', img)
+img = cv.imread(r'Resources/Faces/val/elton_john/1.jpg')
+cv.imshow('Person', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Ben Gray', gray)
+cv.imshow('Person Gray', gray)
 
 faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
 for (x,y,w,h) in faces_rect:
     faces_roi = gray[y:y+h, x:x+w]
-    cv.imshow('Ben faces_roi', faces_roi)
+    cv.imshow('Person faces_roi', faces_roi)
+
+    label, confidence = face_recognizer.predict(faces_roi)
+    print(f'Label = {people[label]} with a confidence of {confidence}')
+
+    cv.putText(img, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
+    cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
+
+cv.imshow('Detected faces', img)
 
 
 cv.waitKey(0)
