79f5fae5c03f72b28e87fea7b5fd90fc487ce948
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 09:38:08 -0700

    Masked Circle Image

--- masking.py
+++ masking.py
@@ -5,7 +5,10 @@ img: np.ndarray = cv.imread('Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
 blank = np.zeros(img.shape[:2], dtype='uint8')
-mask = cv.circle(blank, (img.shape[1]//2,img.shape[0]//2), 100, 255, -1)
-cv.imshow('Mask', mask)
+mask = cv.circle(blank, (img.shape[1]//2+60,img.shape[0]//2), 100, 255, -1)
+cv.imshow('Mask circle', mask)
+
+masked = cv.bitwise_and(img, img, mask=mask)
+cv.imshow('Masked Circle Image', masked)
 
 cv.waitKey(0)
