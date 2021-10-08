e0e1c336fb374770161af842be7deb59926c34ee
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 10:16:06 -0700

    Inverse thresholding

--- threshold.py
+++ threshold.py
@@ -8,8 +8,13 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
 # Simple thresholding
-# THRESH_BINARY =if pixel > 150 set to 255 else 0
+# THRESH_BINARY = if pixel > 150 set to 255 else 0
 threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
 cv.imshow('Simple thresholding', thresh)
+
+# Inverse thresholding
+# THRESH_BINARY_INV = if pixel < 150 set to 255 else 0
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+cv.imshow('Inverse thresholding', thresh)
 
 cv.waitKey(0)
