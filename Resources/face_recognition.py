44c74aaf0ea9e23a0d1a6ad40df06a2303fffc4a
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 11:33:44 -0700

    choose one photo and convert to gray

--- Resources/face_recognition.py
+++ Resources/face_recognition.py
@@ -9,3 +9,14 @@ labels = np.load('labels.npy')
 
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('face_trained.yml')
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+
+img = cv.imread(r'Resources/Faces/val/ben_afflek/2.jpg')
+cv.imshow('Ben', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Ben Gray', gray)
+
+
+cv.waitKey(0)
