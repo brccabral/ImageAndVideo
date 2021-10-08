4bf82a6f93e34ad1cea7f898c7f244509fce0da2
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 09:30:52 -0700

    bitwise OR

--- bitwise.py
+++ bitwise.py
@@ -13,5 +13,9 @@ cv.imshow('Circle', circle)
 bitwise_and = cv.bitwise_and(rectangle, circle)
 cv.imshow('Bitwise AND', bitwise_and)
 
+# bitwise OR
+bitwise_or = cv.bitwise_or(rectangle, circle)
+cv.imshow('Bitwise OR', bitwise_or)
+
 
 cv.waitKey(0)
