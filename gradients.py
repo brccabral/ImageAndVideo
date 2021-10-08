da1b6c17f234083859b09877711d9e8e586f51a4
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 10:28:35 -0700

    Sobel

--- gradients.py
+++ gradients.py
@@ -12,4 +12,11 @@ lap = cv.Laplacian(gray, cv.CV_64F) # this computes differences
 lap = np.uint8(np.absolute(lap)) # images can't have negative numbers
 cv.imshow('Laplacian', lap)
 
+# Sobel
+# cumputes differences in one axis
+sobelx = cv.Sobel(gray, cv.CV_64F, 1, 0)
+cv.imshow('Sobel X', sobelx)
+sobely = cv.Sobel(gray, cv.CV_64F, 0, 1)
+cv.imshow('Sobel Y', sobely)
+
 cv.waitKey(0)
