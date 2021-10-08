1d792bb7ffa7196d9aaaaff759b374e4c6ad7327
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 09:55:06 -0700

    Grayscale masked histogram

--- histogram.py
+++ histogram.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-img: np.ndarray = cv.imread('Resources/Photos/cats 2.jpg')
+img: np.ndarray = cv.imread('Resources/Photos/cats.jpg')
 cv.imshow('Cats', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -18,5 +18,24 @@ plt.ylabel('# of pixels')
 plt.plot(gray_hist)
 plt.xlim([0,256])
 plt.show()
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+circle = cv.circle(blank.copy(), (img.shape[1]//2,img.shape[0]//2), 100, 255, -1)
+cv.imshow('Mask circle', circle)
+
+masked = cv.bitwise_and(gray, gray, mask=circle)
+cv.imshow('Masked Circle Image', masked)
+
+# Grayscale masked histogram
+gray_hist = cv.calcHist([gray], [0], mask=masked, histSize=[256], ranges=[0,256])
+
+plt.figure()
+plt.title('Grayscale circle Histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of pixels')
+plt.plot(gray_hist)
+plt.xlim([0,256])
+plt.show()
+
 
 cv.waitKey(0)
