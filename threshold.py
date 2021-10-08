9bd2bce2153d13e948a9cdd2ebd72849b418d9d7
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 10:14:52 -0700

    Simple thresholding

--- threshold.py
+++ threshold.py
@@ -7,4 +7,9 @@ cv.imshow('Cats', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+# Simple thresholding
+# THRESH_BINARY =if pixel > 150 set to 255 else 0
+threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+cv.imshow('Simple thresholding', thresh)
+
 cv.waitKey(0)
