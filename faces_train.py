c2a03121aaaa5a8d4ea1f45cdafa18f0e9d139b5
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 11:15:02 -0700

    verify the faces detected for first person

--- faces_train.py
+++ faces_train.py
@@ -12,6 +12,8 @@ print(people)
 features = []
 labels = []
 
+haar_cascade: cv.CascadeClassifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 def create_train():
     for person in people:
         path = os.path.join(DIR, person)
@@ -19,7 +21,17 @@ def create_train():
 
         for img in os.listdir(path):
             img_path = os.path.join(path, img)
-            print(img_path)
+            # print(img_path)
             img_array = cv.imread(img_path)
 
+            gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+            for (x,y,w,h) in faces_rect:
+                faces_roi = img_array[y:y+h, x:x+w]
+                cv.imshow(f'{person} {img}', faces_roi)
+        break
+
 create_train()
+
+cv.waitKey(0)
