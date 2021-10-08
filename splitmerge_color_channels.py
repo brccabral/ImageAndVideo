3564580be7cbb3518a43bb151ff990f20970f1d3
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 08:45:08 -0700

    colored blue green red

--- splitmerge_color_channels.py
+++ splitmerge_color_channels.py
@@ -18,4 +18,13 @@ print(r.shape)
 merged = cv.merge([b,g,r])
 cv.imshow('Merged', merged)
 
+blank = np.zeros(img.shape[:2],dtype='uint8')
+blue = cv.merge([b,blank,blank])
+green = cv.merge([blank,g,blank])
+red = cv.merge([blank,blank,r])
+
+cv.imshow('Blue channel', blue)
+cv.imshow('Green channel', green)
+cv.imshow('Red channel', red)
+
 cv.waitKey(0)
