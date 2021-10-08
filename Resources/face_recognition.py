d926f9d4221546b47898a2e221179bed0b36bdea
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 11:35:19 -0700

    detect a face in the gray picture

--- Resources/face_recognition.py
+++ Resources/face_recognition.py
@@ -18,5 +18,10 @@ cv.imshow('Ben', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Ben Gray', gray)
 
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+for (x,y,w,h) in faces_rect:
+    faces_roi = gray[y:y+h, x:x+w]
+    cv.imshow('Ben faces_roi', faces_roi)
+
 
 cv.waitKey(0)
