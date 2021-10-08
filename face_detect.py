0e38cf98d944c65df3bfe340b60815b37f08fae5
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 10:54:10 -0700

    get number of faces

--- face_detect.py
+++ face_detect.py
@@ -7,4 +7,8 @@ cv.imshow('Person', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+haar_cascade: cv.CascadeClassifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+print(f'Number of faces found = {len(faces_rect)}')
+
 cv.waitKey(0)
