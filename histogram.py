13d070373d7ea94fdfc2ac584f3531fc8f3110ef
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 09:46:39 -0700

    start histogram

---
+++ histogram.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+import numpy as np
+
+img: np.ndarray = cv.imread('Resources/Photos/cats.jpg')
+cv.imshow('Cats', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+
+cv.waitKey(0)
