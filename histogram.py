54ef84801062c774c94cc7573913087ef8a51244
Bruno Cabral <cabral.brc@gmail.com>
2021-10-08 09:50:26 -0700

    Grayscale histogram

--- histogram.py
+++ histogram.py
@@ -1,11 +1,22 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 
-img: np.ndarray = cv.imread('Resources/Photos/cats.jpg')
+img: np.ndarray = cv.imread('Resources/Photos/cats 2.jpg')
 cv.imshow('Cats', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+# Grayscale histogram
+gray_hist = cv.calcHist([gray], [0], mask=None, histSize=[256], ranges=[0,256])
+
+plt.figure()
+plt.title('Grayscale Histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of pixels')
+plt.plot(gray_hist)
+plt.xlim([0,256])
+plt.show()
 
 cv.waitKey(0)
